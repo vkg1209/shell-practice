fa72672f6865dbc23c1ba5155ae6e39050b17000
vkg1209 <vinayakumargantala@gmail.com>
2025-11-22 17:47:13 +0530

    disk utilization

---
+++ disk-utilization.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DISK_UTIL=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2
+IP_ADDRESS=$($(curl -s http://169.254.169.254/latest/meta-data/local-ipv4))
+MESSAGE_BODY=""
+
+while IFS= read -r line
+do
+    USAGE= $(echo $DISK_UTIL | awk '{print $6}' | cut -d "%d" -f1)
+    PARTITION=$(echo $DISK_UTIL | awk '{print $2}')
+
+    if [ $USAGE -gt $DISK_THRESHOLD ]; then
+        MESSAGE_BODY+="High Disk Usage: $USAGE%"
+        
+done <<< $DISK_UTIL
